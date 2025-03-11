0ece25f60bd9fb542950b14eaaf569d44a937107
Saul Gomes <saulgdev@proton.me>
2025-03-11 18:32:04 -0300

    feat: adiciona os arquivos de keymaps e configs

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Select All
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Split window
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end)
